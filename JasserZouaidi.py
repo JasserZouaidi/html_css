dc1a6df03b780a37652dbd301071e24c2f687553
JasserZouaidi <99995087+JasserZouaidi@users.noreply.github.com>
2022-12-04 19:16:40 +0100

    Update JasserZouaidi.py

--- JasserZouaidi.py
+++ JasserZouaidi.py
@@ -11,48 +11,38 @@ def remplir1 (f1) :
         r = input('Continuez (O/N) ?')
         while r.upper() not in {"O","N"} :
             r = input('Continuez (O/N) ?')
-    f1.close
+    f1.close()
 
-
-
-def one (x) :
-    ch = str(x)
+def one (ch) :
     r = 0
-    for i in range (len(ch)) :
+    for i in range (5) :
         if ch[i] == "1" :
             r += 1
     return r
 
-
-
-
 def tri(t,n):
     for i in range(n):
-        max = i
-    for j in range(i+1,n):
-        if one(t[max]) < one(t[j]):
-            max = j
-    if i != max :
-        a = t[i]
-        t[i] = t[max]
-        t[max] = a
+        m = i
+        for j in range(i+1,n):
+            if one(t[m]) < one(t[j]):
+                m = j
+        if i != m :
+            a = t[i]
+            t[i] = t[m]
+            t[m] = a
 
-def remplir2 (f1,f2) :
+def remplir2  (f1,f2) :
     f1 = open('binaire.txt',"r")
     l = f1.readline()
-    print(l)
     while l != "" :
-        i = 0
         ch = ""
-        t = array([int]*100)
-        while l != "" :
-            t[i] = l[0:5]
-            l = l[6::]
-            i += 1
-        tri(t,i)
-        for j in range (i) :
-            ch += str(t[i]) + " "
-        print(ch)
+        t = array([int]*4)
+        for i in range (4) :
+            t[i] = l[:5]
+            l = l[6:]
+        tri(t,4)
+        for i in range (4) :
+            ch += t[i] + " "
         f2.write(ch + "\n")
         l = f1.readline()
     f1.close()
@@ -60,7 +50,7 @@ def remplir2 (f1,f2) :
 
         
 def afficher(f2) :
-    f2 =open('res.txt',"r")
+    f2 = open('res.txt',"r")
     ch = f2.readline()
     while ch != "" :
         print(ch)
@@ -74,6 +64,6 @@ from numpy import array
 from random import randint
 f1 = open('binaire.txt',"w")
 remplir1(f1)
-f2 =open('res.txt',"w")
+f2 = open('res.txt',"w")
 remplir2(f1,f2)
 afficher(f2)
